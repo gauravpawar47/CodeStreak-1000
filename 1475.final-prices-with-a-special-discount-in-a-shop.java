baf8301023364d4bd9de427108fc8f2e9795af95
gauravpawar47 <gauravpawar883@gmail.com>
2024-12-18 19:52:37 +0530

    18/12/24 (EASY)

---
+++ 1475.final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1475 lang=java
+ *
+ * [1475] Final Prices With a Special Discount in a Shop
+ */
+
+// @lc code=start
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        
+    }
+}
+// @lc code=end
+
