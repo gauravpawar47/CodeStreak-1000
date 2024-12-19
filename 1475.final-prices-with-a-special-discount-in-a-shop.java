450fd6d5059c7a93e3c23b1e066584cc3df6d7ec
gauravpawar47 <gauravpawar883@gmail.com>
2024-12-19 23:25:01 +0530

    19/12/24 (MEDIUM)

--- 1475.final-prices-with-a-special-discount-in-a-shop.java
+++ 1475.final-prices-with-a-special-discount-in-a-shop.java
@@ -5,9 +5,28 @@
  */
 
 // @lc code=start
-class Solution {
-    public int[] finalPrices(int[] prices) {
+class Solution
+{
+    public int[] finalPrices(int[] prices) 
+    {
+        int n = prices.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = prices[i];
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (prices[j] <= prices[i])
+                {
+                    res[i] -= prices[j];
+                    break;
+                }
+            }
+        }
         
+        return res;
     }
 }
 // @lc code=end
